be7d98c8b416a293bb22f60a55f415e36c5fb322
Alexandre Lubeck <alexandrelubeck@gmail.com>
2024-05-17 14:57:30 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    
+    $('#telefone').mask('( 00 ) 00000 - 0000')
+    $('#cpf').mark('000.000.000-00')
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+            rules: {
+                nome:{
+                    required: true
+            },
+                email: {
+                    required: true,
+                    email: true
+            },
+                telefone: {
+                    required: true
+            },
+                cpf: {
+                    required: true
+            },
+                endereco: {
+                    required: true
+            },
+                cep: {
+                    required: true
+            }
+        },
+        messages: {
+            nome:'Por favor, insira seu nome',
+            email:'Por favor, insira seu email',
+            telefone:'Por favor, insira seu telefone',
+            cpf:'Por favor, insira seu CPF',
+            endereco:'Por favor, insira seu endereço completo',
+            cep:'Por favor, insira seu CEP'
+        }
+    })
+})
